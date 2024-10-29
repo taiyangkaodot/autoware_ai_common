ab9274ffdf43d772051759a80906a4f2ee010603
taiyangkaodot <taiyangkao@proton.me>
2024-10-29 10:22:58 +0800

    Update autoware_build_flags-extras.cmake

--- autoware_build_flags/cmake/autoware_build_flags-extras.cmake
+++ autoware_build_flags/cmake/autoware_build_flags-extras.cmake
@@ -18,9 +18,9 @@ macro(AW_CHECK_CUDA)
     find_package(CUDA REQUIRED)
     find_package(Eigen3 REQUIRED)
 
-    if(NOT ${CUDA_VERSION} VERSION_LESS "10.0"
-            AND NOT ${CUDA_VERSION} VERSION_EQUAL "10.0" )
-      message(FATAL_ERROR "GPU support on Melodic requires CUDA<=10.0")
+    if(NOT ${CUDA_VERSION} VERSION_LESS "12.0"
+            AND NOT ${CUDA_VERSION} VERSION_EQUAL "12.0" )
+      message(FATAL_ERROR "GPU support on Melodic requires CUDA<=12.0")
     endif()
     if(${CUDA_VERSION} VERSION_GREATER "9.1"
           AND ${CMAKE_VERSION} VERSION_LESS "3.12.3")
